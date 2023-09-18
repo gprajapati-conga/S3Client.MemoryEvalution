fc02bae6fcd466a9da18f617a6d4765c395518d3
gprajapati-conga <63354552+gprajapati-conga@users.noreply.github.com>
2023-09-18 22:27:08 +0530

    adding docker-compose support

---
+++ AWS.Nuget.MemoryValidation/Controllers/S3ClientController.cs
@@ -0,0 +1,49 @@
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace AWS.Nuget.MemoryValidation.Controllers
+{
+    [ApiController]
+    [Route("api/aws/v1/[controller]")]
+    public class S3ClientController : ControllerBase
+    {
+
+        private readonly ILogger<S3ClientController> _logger;
+        private IAmazonS3 amazonS3Client;
+
+        public S3ClientController(ILogger<S3ClientController> logger)
+        {
+            var amazonS3Config = new AmazonS3Config()
+            {
+                MaxErrorRetry = Constants.MaxRetryAttempts,
+                ServiceURL = Constants.ServiceURL
+            };
+            _logger = logger;
+            amazonS3Client = new AmazonS3Client(Constants.AccessKey, Constants.SecretKey, amazonS3Config);
+        }
+
+        [HttpGet("putobject")]
+        public async Task<IActionResult> PutObjectAsync(int numberOfRequests)
+        {
+            Stream fileStream = new FileStream(Constants.InputFileName, FileMode.Open);
+            byte[] byteArray = new byte[fileStream.Length];
+            _ = fileStream.Read(byteArray, 0, byteArray.Length);
+
+            var tasks = new List<Task>();
+            for (int j = 0; j < numberOfRequests; j++)
+            {
+                tasks.Add(amazonS3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest()
+                {
+                    InputStream = JsonSerializer.Deserialize<MemoryStream>(byteArray),
+                    Key = $"{Constants.BasePath}/{Guid.NewGuid()}",
+                    BucketName = Constants.BucketName
+                }));
+            }
+            await Task.WhenAll(tasks);
+            
+            return Ok($"Completed {numberOfRequests} S3Client.PutObjectAsync requests");
+        }
+    }
+}
