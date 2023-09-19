ff00d6722092d28d85ae9dff9fbab66f3cc34b28
gprajapati-conga <63354552+gprajapati-conga@users.noreply.github.com>
2023-09-19 23:58:07 +0530

    Update Constants.cs

--- AWS.Nuget.MemoryValidation/Constants.cs
+++ AWS.Nuget.MemoryValidation/Constants.cs
@@ -6,7 +6,7 @@
         public const string SecretKey = "****************";
         public const string ServiceURL = "https://s3.<<awa-region>>.amazonaws.com";
         public const int MaxRetryAttempts = 3;
-        public const string BucketName = "s3client-memorytest";
+        public const string BucketName = "****************";
         public const string BasePath = "data";
         public const string InputFileName = @"Data/500bytes.json";
     }
